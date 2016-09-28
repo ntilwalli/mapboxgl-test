fad908b5fa8e408d6e896cf1dee2018d9cb02554
Nikhil Tilwalli <ntilwalli@gmail.com>
2016-09-27 22:50:21 -0400

    remove postgis extensions except main one for now to test heroku

--- apps/shared/priv/repo/migrations/20160828000132_create_postgis_extension.exs
+++ apps/shared/priv/repo/migrations/20160828000132_create_postgis_extension.exs
@@ -3,19 +3,19 @@ defmodule Shared.Repo.Migrations.CreatePostgisExtension do
 
   def up do
     execute("CREATE EXTENSION postgis;")
-    execute("CREATE EXTENSION postgis_sfcgal;")
-    execute("CREATE EXTENSION fuzzystrmatch;")
-    execute("CREATE EXTENSION address_standardizer;")
-    execute("CREATE EXTENSION address_standardizer_data_us;")
-    execute("CREATE EXTENSION postgis_tiger_geocoder;")
+    # execute("CREATE EXTENSION postgis_sfcgal;")
+    # execute("CREATE EXTENSION fuzzystrmatch;")
+    # execute("CREATE EXTENSION address_standardizer;")
+    # execute("CREATE EXTENSION address_standardizer_data_us;")
+    # execute("CREATE EXTENSION postgis_tiger_geocoder;")
   end
 
   def down do
-    execute("DROP EXTENSION postgis_tiger_geocoder;")
-    execute("DROP EXTENSION address_standardizer_data_us;")
-    execute("DROP EXTENSION address_standardizer;")
-    execute("DROP EXTENSION fuzzystrmatch;")
-    execute("DROP EXTENSION postgis_sfcgal;")
+    # execute("DROP EXTENSION postgis_tiger_geocoder;")
+    # execute("DROP EXTENSION address_standardizer_data_us;")
+    # execute("DROP EXTENSION address_standardizer;")
+    # execute("DROP EXTENSION fuzzystrmatch;")
+    # execute("DROP EXTENSION postgis_sfcgal;")
     execute("DROP EXTENSION postgis;")
   end;
 end
