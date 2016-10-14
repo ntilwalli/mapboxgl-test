b3bdc0f2f3472064090f0d7f1bb86c1fe49fbffd
Nikhil Tilwalli <ntilwalli@gmail.com>
2016-10-14 10:10:23 -0400

    Remove cyclejs from custom_deps

--- custom_deps/cyclejs/http/lib/http-driver.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="core-js" />
-import { Stream, MemoryStream } from 'xstream';
-import { ResponseStream, RequestOptions, RequestInput, Response } from './interfaces';
-export declare function optionsToSuperagent(rawReqOptions: RequestOptions): any;
-export declare function createResponse$(reqInput: RequestInput): Stream<Response>;
-export declare type ResponseMemoryStream = MemoryStream<Response> & ResponseStream;
-export declare function makeHTTPDriver(): Function;
--- custom_deps/cyclejs/http/lib/isolate.d.ts
+++
@@ -1,6 +0,0 @@
-import { HTTPSource, RequestOptions, RequestInput } from './interfaces';
-export interface Mappable<T, R> {
-    map(project: (x: T) => R): Mappable<R, any>;
-}
-export declare function isolateSource(httpSource: HTTPSource, scope: string): HTTPSource;
-export declare function isolateSink(request$: Mappable<RequestInput | string, RequestOptions>, scope: string): any;
