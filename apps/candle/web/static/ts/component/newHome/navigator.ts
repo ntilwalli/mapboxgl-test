5c3433e6a6bd3ffd8b13fa6e034a559e28476b8c
Nikhil Tilwalli <ntilwalli@gmail.com>
2017-01-01 12:39:03 -0500

    home menu changes

--- apps/candle/web/static/ts/component/newHome/navigator.ts
+++ apps/candle/web/static/ts/component/newHome/navigator.ts
@@ -3,6 +3,15 @@ import {div, span, button} from '@cycle/dom'
 import Immutable = require('immutable')
 import {combineObj} from '../../utils'
 
+const routes = [
+  {pattern: /^\/profile$/, value: {type: 'success', data: 'profile'}},
+  {pattern: /^\/listings$/, value: {type: 'success', data: 'listings'}},
+  {pattern: /^\/messages$/, value: {type: 'success', data: 'messages'}},
+  {pattern: /^\/settings$/, value: {type: 'success', data: 'settings'}},
+  {pattern: /^\/$/, value: {type: 'success', data: 'profile'}},
+  {pattern: /.*/, value: {type: "error"}}
+]
+
 function intent(sources) {
   const {DOM} = sources
 
@@ -15,7 +24,7 @@ function intent(sources) {
   const brand_button$ = DOM.select(`.appBrandButton`).events(`click`)
 
   return {
-    selected$: O.merge(
+    page$: O.merge(
       listings$, messages$, settings$, calendar$, profile$
     ),
     show_menu$, 
@@ -24,8 +33,9 @@ function intent(sources) {
 }
 
 function reducers(actions, inputs) {
-  const selected_r = actions.selected$.map(val => state => state.set('selected', val))
-  return O.merge(selected_r)
+  //const page_r = actions.page$.map(val => state => state.set('page', val))
+  //return O.merge(page_r)
+  return O.never()
 }
 
 function model(actions, inputs) {
@@ -36,7 +46,7 @@ function model(actions, inputs) {
     .switchMap((info: any) => {
       const {props} = info
       const init = {
-        selected: props || 'profile'
+        page: props || 'profile'
       }
       return reducer$
         .startWith(Immutable.Map(init))
@@ -49,15 +59,15 @@ function model(actions, inputs) {
 function view(state$) {
   return state$.map(state => {
     console.log('state', state)
-    const {selected} = state
-    const listings_class = selected === 'listings' ? '.selected' : '.not-selected'
-    const messages_class = selected === 'messages' ? '.selected' : '.not-selected'
-    const settings_class = selected === 'settings' ? '.selected' : '.not-selected'
-    const calendar_class = selected === 'calendar' ? '.selected' : '.not-selected'
-    const profile_class = selected === 'profile' ? '.selected' : '.not-selected'
-    return div('.user-navigator.d-flex.fx-j-sb', [
-      button('.appBrandButton.h-2.hopscotch-icon.btn.btn-link.nav-brand', []),
-      span([
+    const {page} = state
+    const listings_class = page === 'listings' ? '.selected' : '.not-selected'
+    const messages_class = page === 'messages' ? '.selected' : '.not-selected'
+    const settings_class = page === 'settings' ? '.selected' : '.not-selected'
+    const calendar_class = page === 'calendar' ? '.selected' : '.not-selected'
+    const profile_class = page === 'profile' ? '.selected' : '.not-selected'
+    return div('.d-flex.fx-j-sb', [
+      button('.appBrandButton.h-2.mw-2.hopscotch-icon.btn.btn-link.nav-brand', []),
+      span('.user-navigator', [
         span(profile_class, [button('.hidden-md-up.btn.btn-link.appProfileButton.menu-item', [span('.fa.fa-user', [])])]),
         span(listings_class, [button('.hidden-md-up.btn.btn-link.appListingsButton.menu-item', [span('.fa.fa-microphone', [])])]),
         span(messages_class, [button('.hidden-md-up.btn.btn-link.appMessagesButton.menu-item', [span('.fa.fa-envelope', [])])]),
@@ -74,17 +84,57 @@ function view(state$) {
   })
 }
 
+function muxRouter(sources) {
+  const {Router} = sources
+  const route$ = Router.define(routes)
+    .publishReplay(1).refCount()
+  const valid_path$ = route$.filter(route => route.value.info.type === 'success')
+    .map(route => {
+      return route.value.info.data
+    })
+    .publishReplay(1).refCount()
+  const invalid_path$ = route$.filter(route => route.value.info.type === 'error')
+    .publishReplay(1).refCount()
+
+  return {
+    valid_path$,
+    invalid_path$
+  }
+}
+
 export default function main(sources, inputs) {
+  const muxed_router = muxRouter(sources)
   const actions = intent(sources)
-  const state$ = model(actions, inputs)
+  const state$ = model(actions, {...inputs, props$: muxed_router.valid_path$})
   const vtree$ = view(state$)
 
   return {
     DOM: vtree$,
-    Router: actions.brand_button$.mapTo('/'),
-    MessageBus: O.merge(
-      actions.show_menu$.mapTo({to: `main`, message: `showLeftMenu`}),
+    Router: O.merge(
+      muxed_router.invalid_path$.mapTo({
+        type: 'replace',
+        action: 'REPLACE',
+        pathname: '/home'
+      }),
+      actions.page$.map(page => {
+        const out = {
+          type: 'replace',
+          action: 'REPLACE',
+          pathname: page === 'profile' ? '/home' : '/home/' + page
+        }
+
+        return out  
+      })
     ),
-    output$: state$.pluck('selected')
+    MessageBus: O.merge(
+      actions.show_menu$.withLatestFrom(state$, (_, state) => ({
+        to: `main`, message: {
+          type: `showLeftMenu`, data: {
+            redirect_url: state.page ? '/home' + state.page : '/home'
+          }
+        }
+      }))
+    ),
+    output$: state$.pluck('page').distinctUntilChanged()
   }
 }
