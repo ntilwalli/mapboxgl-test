9a65f0b7d21c16d55d994d93e334939e5109e565
Nikhil Tilwalli <ntilwalli@gmail.com>
2017-01-10 15:18:25 -0500

    Update to elixir v1.4

--- apps/candle/mix.exs
+++ apps/candle/mix.exs
@@ -8,7 +8,7 @@ defmodule Candle.Mixfile do
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
--- apps/scraper/mix.exs
+++ apps/scraper/mix.exs
@@ -9,7 +9,7 @@ defmodule Scraper.Mixfile do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       elixirc_paths: ["lib", "web"],
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
